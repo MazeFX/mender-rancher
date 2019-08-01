0bade0a06b6d453a6121c256bb6473053e5d7705
nickgeense <n.geense@solufo.com>
2019-08-01 11:16:22 +0200

    Added certifacte mounting for api gateway

--- secrets-provider/entrypoint.sh
+++ secrets-provider/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir -p /run/secrets
+mkdir -p /var/www/mendersoftware/cert/
 
 IFS="
 "
@@ -8,7 +8,13 @@ for ENV_VAR in `env`; do
     ENV_VAR=`echo "$ENV_VAR" | sed -e 's,=.*,,'`
     echo $ENV_VAR | grep -q "_SECRET$" || continue
     SECRET_FILE=`echo "$ENV_VAR" | sed -e 's,_SECRET$,,g'`
-    printenv $ENV_VAR > /run/secrets/$SECRET_FILE
-    echo "Providing secrets for $ENV_VAR in /run/secrets/$SECRET_FILE"
+    if [[ $ENV_VAR == *"_CERT"* ]]; then
+        printenv $ENV_VAR > /var/www/mendersoftware/cert/cert.crt
+        echo "Providing secrets for $ENV_VAR in /var/www/mendersoftware/cert/cert.crt"
+    fi
+    if [[ $ENV_VAR == *"_PRIV_KEY"* ]]; then
+        printenv $ENV_VAR > /var/www/mendersoftware/cert/private.key
+        echo "Providing secrets for $ENV_VAR in /var/www/mendersoftware/cert/private.key"
+    fi
 done
 
