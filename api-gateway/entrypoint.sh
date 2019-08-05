873743e01fa5d0e0dafa325cf86c401967d4bcb7
nickgeense <n.geense@solufo.com>
2019-08-05 14:33:00 +0200

    Added certifacte mounting for api gateway

--- api-gateway/entrypoint.sh
+++ api-gateway/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+SOURCE="$CERT_LOCTION/$CERT_NAME"
+TARGET="/var/www/mendersoftware/cert/"
+
+ln $SOURCE/fullchain.pem $TARGET/cert.crt
+echo "Creating Link $SOURCE/fullchain.pem -> $TARGET/cert.crt" >&2
+
+ln $SOURCE/privkey.pem $TARGET/private.key
+echo "Creating Link $SOURCE/privkey.pem -> $TARGET/private.key" >&2
+
 exec /usr/local/openresty/bin/openresty -g "daemon off;" $*
